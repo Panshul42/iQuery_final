0cd2052522f96b5a88c42741bb401990c081a738
Panshul Sharma <panshul@192.168.1.8>
2024-06-07 15:03:11 +0800

    edit src/app/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 /* eslint-enable react/no-unescaped-entities */
 
-import Image from "next/image";
-import Link from 'next/link';
 import InputBox from '@/components/inputbox';
-import FileUpload from '@/components/file-upload';
 
 export default function Home() {
   return (
-    //editedclasses
     <div className="centered-container">
       <div className="center-content editmaxwidth">
         <div className="border rounded border-white p-5 mx-9 my-6">
